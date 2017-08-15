76dd954878ae8941a36052a70366c9dcb0de8031
verdantpath <m_dittmann@yahoo.com>
2017-08-14 21:59:02 -0700

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 (function($) { //function to create private scope with $ parameter
-  // selects only the <a>'s that are children of <li>'s, not the children of <span>'s
+  // selects only the <a>'s that are children of <li>'s, not the children of a <span>. A child is a direct descendant only.
   $('#nav li > a').addClass('selected');
 })(jQuery); // invoke nameless function and pass it the jQuery object
